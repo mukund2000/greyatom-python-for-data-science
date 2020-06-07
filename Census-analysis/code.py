ad65d25b2a6ea562e11988dad3375054940cdb96
mukund2000 <mukund2000@users.noreply.github.com>
2020-06-07 23:03:15 +0530

    Code file created.

---
+++ Census-analysis/code.py
@@ -0,0 +1,50 @@
+# --------------
+# Importing header files
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
+
+#New record
+new_record=[[50,  9,  4,  1,  0,  0, 40,  0]]
+
+#Reading file
+data = np.genfromtxt(path, delimiter=",", skip_header=1)
+
+#Code starts here
+census=np.concatenate([data,new_record])
+census.shape
+age=census[:,0]
+print(age)
+max_age=np.max(age)
+print(max_age)
+min_age=np.min(age)
+age_mean=np.mean(age)
+age_std=np.std(age)
+print(min_age,age_mean,age_std)
+race_0=census[census[:,2]==0]
+race_1=census[census[:,2]==1]
+race_2=census[census[:,2]==2]
+race_3=census[census[:,2]==3]
+race_4=census[census[:,2]==4]
+len_0=len(race_0)
+len_1=len(race_1)
+len_2=len(race_2)
+len_3=len(race_3)
+len_4=len(race_4)
+min_race=min(len_0,len_1,len_2,len_3,len_4)
+minority_race=3
+print(minority_race)
+senior_citizens=census[census[:,0]>60]
+working_hours_sum=senior_citizens.sum(axis=0)[6]
+senior_citizens_len=len(senior_citizens)
+avg_working_hours=working_hours_sum/senior_citizens_len
+print(working_hours_sum,avg_working_hours)
+high=census[census[:,1]>10]
+low=census[census[:,1]<=10]
+avg_pay_high=np.mean(high[:,7])
+avg_pay_low=np.mean(low[:,7])
+print(avg_pay_high,avg_pay_low)
+
+
+
